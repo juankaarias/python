385f6b762fb46102093bc4613e6f6048911432bf
Juan Arias <9106juan@gmail.com>
2022-09-12 22:02:11 -0500

    clase de python

--- code.py
+++ code.py
@@ -6,10 +6,10 @@
 mensaje = "Hola mundo estoy aprendiendo python"
 print(mensaje)
 
-nombres = "Juan Carlos"
-apellidos = "Arias Velez"
+nombres = "Juan camilo"
+apellidos = "Ariza lopez"
 ciudad = "Santa Marta"
-celular = 3013373276
+celular = 304543436
 print(nombres + " " + apellidos + " " + ciudad)
 print("hola", nombres,apellidos,"de la ciudad",ciudad,"con celular",celular)
 print("hola %s de %s"%(nombres,ciudad))
